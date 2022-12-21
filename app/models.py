e578f638a439699225b4948871c3875a6caeeedd
Bruno Alpachino <brunolalpachino@gmail.com>
2022-12-21 03:57:46 +0300

    created the notes model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# notes model
+class Notes(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    content = models.CharField(max_length=255, null=False)
