9aef69a4779eb7bf28b8d4e442f251a4f33cd6e1
Bruno Alpachino <brunolalpachino@gmail.com>
2022-12-21 04:06:08 +0300

    added the django.dot env in the project

--- app/models.py
+++ app/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Notes(models.Model):
     title = models.CharField(max_length=255, null=False)
     content = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.title
